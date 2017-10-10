33ac5e8f715d4abdd67cc4e26f9460f3e949b63c
Priya Prabhakar <Priya.Prabhakar@LibertyMutual.com>
2017-10-10 09:33:15 -0700

    Adding Trash Work

--- src/main/java/com/goforcode/grocerygallery/controllers/TrashController.java
+++ src/main/java/com/goforcode/grocerygallery/controllers/TrashController.java
@@ -1,30 +1,41 @@
 package com.goforcode.grocerygallery.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.goforcode.grocerygallery.models.Item;
+import com.goforcode.grocerygallery.repositories.ItemRepository;
 
-@Controller
+@RestController
 @RequestMapping("/trash")
 public class TrashController {
 	
-	private List<Item> tempItemList = new ArrayList<Item>();
+	private ItemRepository itemRepo;
+	
+	public TrashController(ItemRepository itemRepo) {
+		this.itemRepo = itemRepo;
+	}
 
 	@GetMapping("/wasted")
 	public List<Item> returnWastedItemsInTrash() {
 		/*Return wasted items in the trash */
-		return tempItemList;
+		List<Item> wastedList;
+		wastedList = itemRepo.findByWasWastedTrue();
+		
+		return wastedList;
+		
 	}
 	
 	@GetMapping("/finished")
 	public List<Item> returnFinishedItemsInTrash() {
 		/*Return finished items in the trash */
-		return tempItemList;
+		List<Item> finishedList;
+		finishedList = itemRepo.findByWasFinishedTrue();
+		
+		return finishedList;
 	}
 	
 }
