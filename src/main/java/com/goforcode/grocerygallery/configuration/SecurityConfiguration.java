cae0f6654f2039dd3202ee758a157f7b71e9df9b
Priya Prabhakar <Priya.Prabhakar@LibertyMutual.com>
2017-10-18 14:59:13 -0700

    updating securityconfig file to allow alexa

--- src/main/java/com/goforcode/grocerygallery/configuration/SecurityConfiguration.java
+++ src/main/java/com/goforcode/grocerygallery/configuration/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.authorizeRequests()
 				.antMatchers(HttpMethod.OPTIONS).permitAll()
-				.antMatchers("/user/signup", "/login").permitAll()
+				.antMatchers("/user/signup", "/login", "/alexa/*").permitAll()
 				.anyRequest().authenticated();
 	}
 	
